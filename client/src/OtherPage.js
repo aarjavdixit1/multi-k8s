b8d1e4deb0da8468c28a0ef27eda7b3b65e7693d
aarjavdixit1 <aarjavdixit@gmail.com>
2023-09-11 19:32:31 -0700

    Commit

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const OtherPage = () => {
   return (
     <div>
-      In some other page!!!
+      Im some other page!
       <Link to="/">Go back home</Link>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
-const keys = require('./keys');
+const keys = require("./keys");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// Express App Setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const { Pool } = require('pg');
+// Postgres Client Setup
+const { Pool } = require("pg");
 const pgClient = new Pool({
-    user : keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
 });
 
 pgClient.on("connect", (client) => {
